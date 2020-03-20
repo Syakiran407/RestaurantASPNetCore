db1a7a88f21b04cf59d437328b25036553ad76ed
Mahmud Syakiran <1821054@chester.ac.uk>
2020-03-21 00:48:33 +0800

    Add all files

---
+++ J72443Restaurant/wwwroot/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var typed = new Typed(".typed", {
+        strings: ["Best Restaurant in UK.", "Ten Times Chef's World Champion.", "Premium Meals."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+});
